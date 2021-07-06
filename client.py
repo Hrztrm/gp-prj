cb3e6a62dc7a3490e1384b69640234036367fa44
Hrztrm <38752407+Hrztrm@users.noreply.github.com>
2021-07-06 23:57:12 +0800

    Update client.py

--- client.py
+++ client.py
@@ -6,36 +6,18 @@ from colorama import Fore, init, Back
 import time
 import sys
 from os import system, name
-# init colors
-init()
 
-# set the available colors
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
-
-# choose a random color for the client
-client_color = random.choice(colors)
-
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
 SERVER_HOST = "192.168.56.105"
 SERVER_PORT = 8888 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
 end = 0
 # initialize TCP socket
 s = socket.socket()
 print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
-# connect to the server
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
 print("\n---------- HELLO PLAYER!! WELCOME TO NO-FEAR GAME ----------\n---- The game whereas only the brave player will survive----")
 print("\n\t****** PLAY TO WIN, BUT ENJOY THE FUN ******")
-# prompt the client for a name
-#name = input("Enter your name: ")
+
 def winner():
     print("\nWith the final encouter, encountered. You left the place with a code in hand. Waiting eagerly for the next adventure to come.\n")
     message = s.recv(1024).decode()
@@ -84,10 +66,5 @@ t2.start()
 while True:
     if end == 1:
         break
-    # input message we want to send to the server
-    # a way to exit the program
-    # add the datetime, name & the color of the sender
-    # finally, send the message
 
-# close the socket
 s.close()
